2785b00eef828c384cf067b7d3f02790fef87213
bx <edlislbx@yeah.net>
2023-03-08 17:27:28 -0500

    add default value

--- frontend/src/view/homepage/index.jsx
+++ frontend/src/view/homepage/index.jsx
@@ -13,32 +13,31 @@ export const HomePage = () => {
   const handleSteadyTime = () => {
     setSteadyTime(!steadyTime);
   };
-
-  const maxTime = useRef(5000);
-  const lambdaClasses = useRef();
-  const muClasses = useRef();
-  const deltaClasses = useRef();
-  const gammaClasses = useRef();
-  const n = useRef();
-  const concurrency = useRef();
-  const delta = useRef();
-  const gamma = useRef();
-  const nc = useRef();
+  const lambdaClasses_1 = useRef(4);
+  const lambdaClasses_2 = useRef(5);
+  const lambdaClasses_3 = useRef(6);
+  const lambdaClasses_4 = useRef(2);
+  const muClasses_1 = useRef(0.1);
+  const muClasses_2 = useRef(0.083);
+  const muClasses_3 = useRef(0.0909);
+  const muClasses_4 = useRef(0.1111);
+  const deltaClasses_1 = useRef(0.3333);
+  const deltaClasses_2 = useRef(0.25);
+  const deltaClasses_3 = useRef(0.5);
+  const deltaClasses_4 = useRef(0.2);
+  const gammaClasses_1 = useRef(0.2);
+  const gammaClasses_2 = useRef(0.0625);
+  const gammaClasses_3 = useRef(0.1111);
+  const gammaClasses_4 = useRef(0.0714);
   const steadyTimeRef = useRef();
 
   const navigate = useNavigate();
   const runSimulation = () => {
     const data = {
-      maxTime: maxTime.current.value,
-      lambdaClasses: lambdaClasses.current.value,
-      muClasses: muClasses.current.value,
-      deltaClasses: deltaClasses.current.value,
-      gammaClasses: gammaClasses.current.value,
-      n: n.current.value,
-      concurrency: concurrency.current.value,
-      delta: delta.current.value,
-      gamma: gamma.current.value,
-      nc: nc.current.value,
+      lambdaClasses: lambdaClasses_1.current.value+","+lambdaClasses_2.current.value+","+lambdaClasses_3.current.value+","+lambdaClasses_4.current.value,
+      muClasses: muClasses_1.current.value+","+muClasses_2.current.value+","+muClasses_3.current.value+","+muClasses_4.current.value,
+      deltaClasses: deltaClasses_1.current.value+","+deltaClasses_2.current.value+","+deltaClasses_3.current.value+","+deltaClasses_4.current.value,
+      gammaClasses: gammaClasses_1.current.value+","+gammaClasses_2.current.value+","+gammaClasses_3.current.value+","+gammaClasses_4.current.value,
     };
     if (steadyTime) {
       data.steadyTime = steadyTimeRef.current.value;
@@ -59,128 +58,25 @@ export const HomePage = () => {
         width: "100%",
       }}
     >
-      <Typography variant="h2" component="h2" sx={{ mt: 3, fontFamily:"cursive"  }}>
+      <Typography
+        variant="h2"
+        component="h2"
+        sx={{ mt: 3, fontFamily: "cursive" }}
+      >
         Simulation
       </Typography>
 
-      <Typography variant="p" component="p" sx={{ mt: 2, fontFamily:"cursive" }}>
-        <b>NOTE:</b> Please enter the values separated by commas for different
-        class
-      </Typography>
-      <Typography variant="p" component="p" sx={{fontFamily:"cursive" }}>
-        for <i>Number of Clients</i>, <i>Average Service Times</i>,{" "}
-        <i>Delay Time</i>, <i>Time Till Recidivism</i>
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-          width: "100%",
-        }}
+      <Typography
+        variant="p"
+        component="p"
+        sx={{ mt: 2,mb:5, fontFamily: "cursive" }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-end",
-            justifyContent: "center",
-            height: "100%",
-            width: "50%",
-            p: 5,
-          }}
-        >
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Maximum simulation time"
-            type="number"
-            variant="filled"
-            inputRef={maxTime}
-          />
+        <b>NOTE:</b> Click the fields to change the values
+      </Typography>
 
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label=" Number of Clients"
-            variant="filled"
-            inputRef={lambdaClasses}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Average Service Times"
-            variant="filled"
-            inputRef={muClasses}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Delay Time"
-            variant="filled"
-            inputRef={deltaClasses}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Time Till Recidivism"
-            variant="filled"
-            inputRef={gammaClasses}
-          />
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "center",
-            height: "100%",
-            width: "50%",
-            p: 5,
-          }}
-        >
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="N"
-            type="number"
-            variant="filled"
-            inputRef={n}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Concurrency"
-            type="number"
-            variant="filled"
-            inputRef={concurrency}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Delta"
-            type="number"
-            variant="filled"
-            inputRef={delta}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Gamma"
-            type="number"
-            variant="filled"
-            inputRef={gamma}
-          />
-
-          <TextField
-            sx={{ width: "50%", mb: 2 }}
-            label="Nc"
-            type="number"
-            variant="filled"
-            inputRef={nc}
-          />
-        </Box>
-      </Box>
+      <Typography variant="p" component="p" sx={{ fontFamily: "cursive" }}>
+        Number of Clients
+      </Typography>
 
       <Box
         sx={{
@@ -192,23 +88,205 @@ export const HomePage = () => {
           width: "100%",
         }}
       >
-        <FormGroup>
-          <FormControlLabel
-            control={<Switch defaultChecked onChange={handleSteadyTime} />}
-            label="Run Fluid Queue Model"
-          />
-        </FormGroup>
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 1"
+          variant="filled"
+          id="outlined-helperText"
+          value={4}
+          inputRef={lambdaClasses_1}
+        />
 
-        {steadyTime && (
-          <TextField
-            sx={{ width: "20%" }}
-            label="Steady Time"
-            type="number"
-            variant="filled"
-            inputRef={steadyTimeRef}
-          />
-        )}
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 2"
+          variant="filled"
+          d="outlined-helperText"
+          value={5}
+          inputRef={lambdaClasses_2}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 3"
+          variant="filled"
+          d="outlined-helperText"
+          value={6}
+          inputRef={lambdaClasses_3}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 4"
+          variant="filled"
+          d="outlined-helperText"
+          value={2}
+          inputRef={lambdaClasses_4}
+        />
       </Box>
+
+      <Typography variant="p" component="p" sx={{ fontFamily: "cursive" }}>
+        Average Length of Stay
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 1"
+          variant="filled"
+          id="outlined-helperText"
+          value={0.1}
+          inputRef={muClasses_1}
+        />
+
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 2"
+          variant="filled"
+          id="outlined-helperText"
+          value={0.083}
+          inputRef={muClasses_2}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 3"
+          variant="filled"
+          id="outlined-helperText"
+          value={0.0909}
+          inputRef={muClasses_3}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 4"
+          variant="filled"
+          id="outlined-helperText"
+          value={0.1111}
+          inputRef={muClasses_4}
+        />
+      </Box>
+
+      <Typography variant="p" component="p" sx={{ fontFamily: "cursive" }}>
+        Delay Time
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          label="Class 1"
+          variant="filled"
+          id="outlined-helperText"
+          value={0.3333}
+          inputRef={deltaClasses_1}
+        />
+
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 2"
+          variant="filled"
+          value={0.25}
+          inputRef={deltaClasses_2}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 3"
+          variant="filled"
+          value={0.5}
+          inputRef={deltaClasses_3}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 4"
+          variant="filled"
+          value={0.2}
+          inputRef={deltaClasses_4}
+        />
+      </Box>
+
+      <Typography variant="p" component="p" sx={{ fontFamily: "cursive" }}>
+        Time Till Recidivism
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "center",
+          height: "100%",
+          width: "100%",
+          mb:5
+        }}
+      >
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 1"
+          variant="filled"
+          value={0.2}
+          inputRef={gammaClasses_1}
+        />
+
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 2"
+          variant="filled"
+          value={0.0625}
+          inputRef={gammaClasses_2}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 3"
+          variant="filled"
+          value={0.1111}
+          inputRef={gammaClasses_3}
+        />
+        <TextField
+          sx={{ width: "10%", m: 2 }}
+          id="outlined-helperText"
+          label="Class 4"
+          variant="filled"
+          value={0.0714}
+          inputRef={gammaClasses_4}
+        />
+      </Box>
+
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch defaultChecked onChange={handleSteadyTime} />}
+          label="Run Fluid Queue Model"
+        />
+      </FormGroup>
+
+      {steadyTime && (
+        <TextField
+          sx={{ width: "20%" }}
+          label="Steady Time"
+          type="number"
+          variant="filled"
+          inputRef={steadyTimeRef}
+        />
+      )}
 
       <Button variant="contained" onClick={runSimulation} sx={{ mt: 6 }}>
         <Typography variant="h5" component="h5">
